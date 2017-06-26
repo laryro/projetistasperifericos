c3e0d40458d778e5d323471c7a53a5eca87c97b4
Laryssa Rocha <laryssa.vrocha@gmail.com>
2017-06-25 21:46:02 -0300

    estiliza imagens do instagram

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ var feed = new Instafeed({
 	accessToken: '4204114729.1677ed0.1b275ac035254427bcc073596a9f8372',
 	sortBy: 'most-recent', 
 	limit: '12',
-	template: '<div class="col-sm-3"><a href="{{link}}"><img src="{{image}}" /></a></div>',
+	template: '<div class="col-sm-3 col-xs-6 instagram-image"><a href="{{link}}"><img class="img-responsive" src="{{image}}" /></a></div>',
 	resolution: 'standard_resolution'
 }); 
 feed.run()

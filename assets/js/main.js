da14dc4338f38b38df08e15edc898fadc90f3a41
Laryssa Rocha <laryssa.vrocha@gmail.com>
2017-06-25 21:38:14 -0300

    teste instafeed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-// var userFeed = new Instafeed({
-//   get: 'user',
-//   userId: 'Inqu4dro_real',
-//   accessToken: '4204114729.ba4c844.b69bb7e7afec486999542568395d8360'
-// });
-// userFeed.run();
-
 var feed = new Instafeed({ 
 	get: 'user', 
 	userId: '4204114729', 
 	accessToken: '4204114729.1677ed0.1b275ac035254427bcc073596a9f8372',
 	sortBy: 'most-recent', 
-	limit: '12' 
+	limit: '12',
+	template: '<div class="col-sm-3"><a href="{{link}}"><img src="{{image}}" /></a></div>'
 }); 
 feed.run()

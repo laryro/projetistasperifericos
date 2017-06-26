6c8299af634a24153c04e8489efe718051e2dc07
Laryssa Rocha <laryssa.vrocha@gmail.com>
2017-06-25 21:30:43 -0300

    teste instafeed

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
 var feed = new Instafeed({ 
 	get: 'user', 
 	userId: 'Inqu4dro_real', 
-	accessToken: '4204114729.ba4c844.b69bb7e7afec486999542568395d8360',
+	accessToken: '46c0976f0d4049f6955ce2a7718615b3',
 	sortBy: 'most-recent', 
 	limit: '12' 
 }); 
